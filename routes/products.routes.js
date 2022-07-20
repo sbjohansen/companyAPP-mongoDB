8390f047b7a535a3659c4f11efb7d6a58db2342f
Slawomir Johansen <baran@fastmail.com>
2022-07-20 18:06:23 +0200

    REMOVE import db

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -2,7 +2,6 @@
 
 const express = require('express');
 const router = express.Router();
-const db = require('./../db');
 const ObjectId = require('mongodb').ObjectId;
 
 router.get('/products', (req, res) => {
